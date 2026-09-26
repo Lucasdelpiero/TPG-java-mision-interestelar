abe061bad283a87bdc519a31b447d39f7cf64441
Lucasdelpiero <lucasdelpiero98@gmail.com>
2026-09-26 17:59:42 -0300

    Packetes movidos dentro de modelo

--- main/src/main/java/modelo/mision/Mision.java
+++ main/src/main/java/modelo/mision/Mision.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package nave;
+package modelo.mision;
 
-
-public class Bitacora {
+public class Mision {
     
 }
--- main/src/main/java/modelo/tripulacion/Tripulante.java
+++ main/src/main/java/modelo/tripulacion/Tripulante.java
@@ -1,10 +1,9 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package nave;
+package modelo.tripulacion;
 
-
-public class Nave {
+public class Tripulante {
     
 }
